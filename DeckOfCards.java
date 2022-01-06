28643cdc5b51885cdbbb84627eb5f5245fb525e1
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-06 10:57:24 +0530

    Update DeckOfCards.java

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,133 +1,132 @@
-    import java.util.ArrayList;
-    import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public class DeckOfCards {
+public class DeckOfCards {
 
-        public ArrayList<String> cardsDeck = new ArrayList<>();
+    public ArrayList<String> cardsDeck = new ArrayList<>();
 
-        public static final Scanner scanner = new Scanner(System.in);
+    public static final Scanner scanner = new Scanner(System.in);
 
-       /*
-        UC1:- Setup the initial game with deck of cards and make sure we have unique cards.
-         */
+   /*
+    UC1:- Setup the initial game with deck of cards and make sure we have unique cards.
+     */
 
-        public void deckOfCards() {
+    public void deckOfCards() {
 
-            String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
-            String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
 
-            int numOfCards = suits.length * ranks.length;
+        int numOfCards = suits.length * ranks.length;
 
-            System.out.println("\nNumber of cards in the deck is : " + numOfCards);
+        System.out.println("\nNumber of cards in the deck is : " + numOfCards);
 
-            // First we have to iterate for loop for ranks starting from index 0.
-            for (int i = 0; i < ranks.length; i++) {
+        // First we have to iterate for loop for ranks starting from index 0.
+        for (int i = 0; i < ranks.length; i++) {
 
-                for (int j = 0; j < suits.length; j++) {
+            for (int j = 0; j < suits.length; j++) {
 
-                    cardsDeck.add(ranks[i] + "----->" + suits[j]);
-                }
+                cardsDeck.add(ranks[i] + "----->" + suits[j]);
             }
-
-            toDisplay(cardsDeck);
         }
-        public static void toDisplay(ArrayList<String> cardsDeck) {
 
-            System.out.println("\nCards in Deck:");
+        toDisplay(cardsDeck);
+    }
+    public static void toDisplay(ArrayList<String> cardsDeck) {
 
-            for (String element : cardsDeck) {
+        System.out.println("\nCards in Deck:");
 
-                System.out.println(element);
+        for (String element : cardsDeck) {
+
+            System.out.println(element);
+
+        }
+
+        System.out.println();
+    }
+    /*
+    UC2 To add no of players in the game
+     */
+
+
+        public void noOfPlayers() {
+
+            System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+
+            int player = scanner.nextInt();
+
+            if (player >= 2 && player <= 4) {
+
+                System.out.println("\n" + player + " players will play the game");
+
+            }
+            else {
+
+                System.out.println("Please enter number of players in the Range");
+
+                noOfPlayers();
 
             }
 
-            System.out.println();
+            scanner.close(); // Closed the Scanner Object.
+
         }
         /*
-        UC2 To add no of players in the game
-         */
+        Use Case 3:-Add the method to sequence the player
+        */
+        public void sequenceOfPlay(int player) {
 
+            System.out.println("\nSequence of cards are below : ");
 
-            public void noOfPlayers() {
+            toshuffle(cardsDeck, player);                      // caling toShuffle Method inside it.
 
-                System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+        }
 
-                int player = scanner.nextInt();
+        public ArrayList<String> toshuffle(ArrayList<String> cardsDeck, int player) {
 
-                if (player >= 2 && player <= 4) {
+            System.out.println("Shuffling the cards before Distribution");
 
-                    System.out.println("\n" + player + " players will play the game");
+            ArrayList<String> temp = new ArrayList<>();
 
-                }
-                else {
+            while (!cardsDeck.isEmpty()) {
 
-                    System.out.println("Please enter number of players in the Range");
+                int loc = (int) (Math.random() * cardsDeck.size());
 
-                    noOfPlayers();
+                temp.add(cardsDeck.get(loc));
 
-                }
-
-                scanner.close(); // Closed the Scanner Object.
-
-            }
-            /*
-            Use Case 3:-Add the method to sequence the player
-            */
-            public void sequenceOfPlay(int player) {
-
-                System.out.println("\nSequence of cards are below : ");
-
-                toshuffle(cardsDeck, player);                      // caling toShuffle Method inside it.
+                cardsDeck.remove(loc);
 
             }
 
-            public ArrayList<String> toshuffle(ArrayList<String> cardsDeck, int player) {
+            cardsDeck = temp;
 
-                System.out.println("Shuffling the cards before Distribution");
+            toDisplay(cardsDeck);                            // To display the cards this method is called.
 
-                ArrayList<String> temp = new ArrayList<>();
+            cardDistribution(cardsDeck, player);             // Calling Card Distribution method inside this method
 
-                while (!cardsDeck.isEmpty()) {
+            return cardsDeck;
 
-                    int loc = (int) (Math.random() * cardsDeck.size());
+        }
+          /*
+        Use Case 4:-Created a method for equal distribution of cards.
+        Every player will be distributed 9 cards.
+        */
+          public static void cardDistribution(ArrayList<String> cardsDeck, int player) {
 
-                    temp.add(cardsDeck.get(loc));
+              // This loop will itterate for no of players
+              for (int i = 0; i < player; i++) {
 
-                    cardsDeck.remove(loc);
+                  System.out.print("\nPlayer " + (i + 1) + " got cards:\n");
 
-                }
+                  // This loop will itterate for no of cards for each player
+                  for (int j = 0; j < 9; j++) {
 
-                cardsDeck = temp;
+                      System.out.print("\t" + cardsDeck.get(i + j * player));
 
-                toDisplay(cardsDeck);                            // To display the cards this method is called.
-
-                cardDistribution(cardsDeck, player);             // Calling Card Distribution method inside this method
-
-                return cardsDeck;
-
-            }
-              /*
-            Use Case 4:-Created a method for equal distribution of cards.
-            Every player will be distributed 9 cards.
-            */
-              public static void cardDistribution(ArrayList<String> cardsDeck, int player) {
-                  
-                  // This loop will itterate for no of players
-                  for (int i = 0; i < player; i++) {
-                      
-                      System.out.print("\nPlayer " + (i + 1) + " got cards:\n");
-                      
-                      // This loop will itterate for no of cards for each player
-                      for (int j = 0; j < 9; j++) {
-                          
-                          System.out.print("\t" + cardsDeck.get(i + j * player));
-                          
-                      }
-                      
                   }
                   
-                  System.out.println();
-                  
               }
-        }
+
+              System.out.println();
+          }
+    }
