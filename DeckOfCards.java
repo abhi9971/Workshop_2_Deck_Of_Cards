8e5d4cf8b6eacdd4903a75d9aee040ed8dacde41
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-06 10:24:58 +0530

    Update DeckOfCards.java

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,28 +1,11 @@
-import java.util.ArrayList;
-
-public class DeckOfCards {
+public class DeckOfCardsMain {
     
-    public ArrayList<String> cardsDeck = new ArrayList<>();
-
-
-    public void deckOfCards() {
+    public static void main(String[] args) {.                       In this I have created an object of DeckOfCardsGame class and just calling the method through object.
         
-        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        DeckOfCards deckOfCardsGame = new DeckOfCards();
         
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
+        deckOfCardsGame.deckOfCards();
         
-        int numOfCards = suits.length * ranks.length;
-        
-        System.out.println("\nNumber of cards in the deck is : " + numOfCards);
-        
-        // First we have to itterate for loop for ranks starting from index 0.
-        for (int i = 0; i < ranks.length; i++) {
-
-            for (int j = 0; j < suits.length; j++) {
-                
-                cardsDeck.add(ranks[i] + "----->" + suits[j]);
-            }
-        }
+        deckOfCardsGame.noOfPlayers();
     }
-
 }
