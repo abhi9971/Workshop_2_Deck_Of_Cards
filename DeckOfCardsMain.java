c549d1452f1b632646c5d7347ac6f60246197688
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-07 10:45:01 +0530

    Added DeckOfCardsMain.java File

---
+++ DeckOfCardsMain.java
@@ -0,0 +1,13 @@
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+		 /*
+        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+         */
+        DeckOfCards deckOfCardsGame = new DeckOfCards();
+        deckOfCardsGame.welcome();
+        deckOfCardsGame.deckOfCards();
+        deckOfCardsGame.noOfPlayers();
+        deckOfCardsGame.sequenceOfPlay(4);
+        deckOfCardsGame.toShuffle();
+    }
+}
