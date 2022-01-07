eb8930ec1de3438d6363c53268ca9eee28f4419e
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-07 10:48:49 +0530

    Update DeckOfCardsMain.java

--- DeckOfCardsMain.java
+++ DeckOfCardsMain.java
@@ -1,13 +1,25 @@
+package com.BridgeLab.WorkShop2.DataStructure.DeckOfCard;
+
+
 public class DeckOfCardsMain {
+	
     public static void main(String[] args) {
-		 /*
+	    
+	 /*
         In this I have created an object of DeckOfCardsGame class and just calling the method through object.
          */
+	    
         DeckOfCards deckOfCardsGame = new DeckOfCards();
+	    
         deckOfCardsGame.welcome();
+	    
         deckOfCardsGame.deckOfCards();
+	    
         deckOfCardsGame.noOfPlayers();
+	    
         deckOfCardsGame.sequenceOfPlay(4);
+	    
         deckOfCardsGame.toShuffle();
+	    
     }
 }
